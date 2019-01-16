21c3f083c93715af2d44f5cdc20022999707291b
Xu Hang <hangxu@antiy.cn>
2019-01-16 13:02:31 +0800

    Add first simple engine

---
+++ engine/activation_functions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Author: Sphantix
+# Mail: sphantix@gmail.cn
+# created time: Wed 16 Jan 2019 12:07:55 PM CST
+
+import numpy as np
+
+# sigmoid函数
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+# 恒等函数
+def identity(x):
+    return x
+
+# softmax函数
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # 溢出对策
+    return np.exp(x) / np.sum(np.exp(x))
