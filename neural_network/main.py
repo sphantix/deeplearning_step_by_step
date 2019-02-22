eca36b8b5da1e66a83d2d2048afb8742240dada7
Xu Hang <hangxu@antiy.cn>
2019-02-22 11:33:01 +0800

    Add accuracy judgement

--- neural_network/main.py
+++ neural_network/main.py
@@ -15,12 +15,18 @@ from two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
 
 # 超参数
-iters_num = 10000
+iters_num = 20000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+
+# 平均每个epoch的迭代次数
+iter_per_epoch = max(train_size/batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -44,13 +50,19 @@ for i in range(iters_num):
     if (i%100 == 0):
         print("iteration({0}: loss: {1})".format(i, loss))
 
+    if (i%iter_per_epoch == 0):
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+
 # 绘制图形
-x = np.arange(len(train_loss_list))
-
-plt.plot(x, train_loss_list)
-plt.xlabel("iteration")
-plt.ylabel("loss")
-plt.ylim(0, 3)
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
 plt.show()
-
-
